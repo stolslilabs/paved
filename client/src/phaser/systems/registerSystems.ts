9ab9a3ba53bf7c0b156475f39aac168aecb73ef8
bal7hazar <bal7hazar@proton.me>
2024-01-16 18:20:51 +0100

    ✨ Upgrade to dojo 0.5.0, fix issue with tile rendering

--- client/src/phaser/systems/registerSystems.ts
+++ client/src/phaser/systems/registerSystems.ts
@@ -2,9 +2,11 @@ import { PhaserLayer } from "..";
 import { mapSystem } from "./mapSystem";
 import { camera } from "./camera";
 import { placeTile } from "./placeTile";
+import { createControlsSystem } from "./createControlsSystem";
 
 export const registerSystems = (layer: PhaserLayer) => {
-    mapSystem(layer);
+    // mapSystem(layer);
     camera(layer);
+    createControlsSystem(layer);
     placeTile(layer);
 };
