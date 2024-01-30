0f6cfdf8cfd6c5562f7714e0a69baf7c77fba96a
bal7hazar <bal7hazar@proton.me>
2024-01-30 15:27:49 +0100

    🐛 Fix dojo config

--- react-app/src/dojo/createSystemCalls.ts
+++ react-app/src/dojo/createSystemCalls.ts
@@ -1,20 +1,12 @@
 import { Account } from "starknet";
-import { Entity, getComponentValue } from "@dojoengine/recs";
-import { uuid } from "@latticexyz/utils";
 import { ClientComponents } from "./createClientComponents";
-import {
-  getEntityIdFromKeys,
-  getEvents,
-  setComponentsFromEvents,
-} from "@dojoengine/utils";
-import { ContractComponents } from "./generated/contractComponents";
 import type { IWorld } from "./generated/generated";
 
 export type SystemCalls = ReturnType<typeof createSystemCalls>;
 
 export function createSystemCalls(
   { client }: { client: IWorld },
-  contractComponents: ContractComponents,
+  contractComponents: any,
   {
     Game,
     Builder,
