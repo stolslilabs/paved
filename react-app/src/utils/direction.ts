dd30f1d1ca00a312752f74cabfa408031263d6da
bal7hazar <bal7hazar@proton.me>
2024-02-10 09:33:33 +0100

    ✨ Add game checks

---
+++ react-app/src/utils/direction.ts
@@ -0,0 +1,11 @@
+export enum Direction {
+    None = "None",
+    NorthWest = "NorthWest",
+    North = "North",
+    NorthEast = "NorthEast",
+    East = "East",
+    SouthEast = "SouthEast",
+    South = "South",
+    SouthWest = "SouthWest",
+    West = "West",
+  }
