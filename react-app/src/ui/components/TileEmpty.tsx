eafec31deccd10ffbfbb8c335bb7ad42bb917c67
bal7hazar <bal7hazar@proton.me>
2024-02-10 10:21:35 +0100

    ✨ Improve neighbors management

--- react-app/src/ui/components/TileEmpty.tsx
+++ react-app/src/ui/components/TileEmpty.tsx
@@ -1,35 +1,139 @@
 import * as THREE from "three";
+import { useDojo } from "@/dojo/useDojo";
+import { useComponentValue } from "@dojoengine/react";
+import { getEntityIdFromKeys } from "@dojoengine/utils";
+import { Entity } from "@dojoengine/recs";
 import { useMemo, useRef, useState, useEffect } from "react";
-import { useGameStore } from "../../store";
-import { getImage, offset, other_offset } from "../../utils";
+import { useGameStore } from "@/store";
+import { getImage, offset, other_offset } from "@/utils";
+import { checkCompatibility } from "@/utils/layout";
 import { createSquareGeometry, getSquarePosition, loader } from "./TileTexture";
+import { useQueryParams } from "@/hooks/useQueryParams";
 
-export const TileEmpty = ({ col, row, size, activeTile }: any) => {
+export const TileEmpty = ({ col, row, size }: any) => {
+  const { gameId } = useQueryParams();
+  const {
+    setup: {
+      clientComponents: { Tile, TilePosition },
+    },
+  } = useDojo();
+
   const squareGeometry = useMemo(() => createSquareGeometry(size), [size]);
   const meshRef = useRef<any>();
-
   const [background, setBackground] = useState<null | string>(null);
   const [texture, setTexture] = useState<THREE.Texture | undefined>(undefined);
   const [rotation, setRotation] = useState(0);
   const [hovered, setHovered] = useState(false);
-
   const {
     orientation,
     selectedTile,
     setSelectedTile,
+    activeEntity,
     hoveredTile,
     setHoveredTile,
     setX,
     setY,
+    setValid,
   } = useGameStore();
+
+  const activeTile = useComponentValue(Tile, activeEntity);
+
+  // const northPosition = useComponentValue(
+  //   TilePosition,
+  //   getEntityIdFromKeys([
+  //     BigInt(gameId),
+  //     BigInt(col),
+  //     BigInt(row + 1),
+  //   ]) as Entity
+  // );
+
+  // const northTile = useComponentValue(
+  //   Tile,
+  //   getEntityIdFromKeys([
+  //     BigInt(gameId),
+  //     BigInt(northPosition?.tile_id || 0),
+  //   ]) as Entity
+  // );
+
+  // const eastPosition = useComponentValue(
+  //   TilePosition,
+  //   getEntityIdFromKeys([
+  //     BigInt(gameId),
+  //     BigInt(col + 1),
+  //     BigInt(row),
+  //   ]) as Entity
+  // );
+
+  // const eastTile = useComponentValue(
+  //   Tile,
+  //   getEntityIdFromKeys([
+  //     BigInt(gameId),
+  //     BigInt(eastPosition?.tile_id || 0),
+  //   ]) as Entity
+  // );
+
+  // const southPosition = useComponentValue(
+  //   TilePosition,
+  //   getEntityIdFromKeys([
+  //     BigInt(gameId),
+  //     BigInt(col),
+  //     BigInt(row - 1),
+  //   ]) as Entity
+  // );
+
+  // const southTile = useComponentValue(
+  //   Tile,
+  //   getEntityIdFromKeys([
+  //     BigInt(gameId),
+  //     BigInt(southPosition?.tile_id || 0),
+  //   ]) as Entity
+  // );
+
+  // const westPosition = useComponentValue(
+  //   TilePosition,
+  //   getEntityIdFromKeys([
+  //     BigInt(gameId),
+  //     BigInt(col - 1),
+  //     BigInt(row),
+  //   ]) as Entity
+  // );
+
+  // const westTile = useComponentValue(
+  //   Tile,
+  //   getEntityIdFromKeys([
+  //     BigInt(gameId),
+  //     BigInt(westPosition?.tile_id || 0),
+  //   ]) as Entity
+  // );
 
   const isSelected = useMemo(() => {
     return selectedTile && selectedTile.col === col && selectedTile.row === row;
   }, [selectedTile]);
 
+  // This state is used to ensure hovered state is updated correctly
   const isHovered = useMemo(() => {
-    return hoveredTile && hoveredTile.col === col && hoveredTile.row === row;
-  }, [hoveredTile]);
+    return (
+      hovered &&
+      hoveredTile &&
+      hoveredTile.col === col &&
+      hoveredTile.row === row
+    );
+  }, [hoveredTile, hovered]);
+
+  // const isValid = useMemo(() => {
+  //   return (
+  //     activeTile &&
+  //     (hovered || isSelected) &&
+  //     checkCompatibility(
+  //       activeTile.plan,
+  //       orientation,
+  //       northTile,
+  //       eastTile,
+  //       southTile,
+  //       westTile
+  //     )
+  //   );
+  // }, [activeTile, orientation, hovered, isSelected]);
 
   useEffect(() => {
     if (background) {
@@ -42,7 +146,6 @@ export const TileEmpty = ({ col, row, size, activeTile }: any) => {
       setTexture(undefined);
     }
   }, [background, rotation, orientation]);
-
   useEffect(() => {
     if (activeTile && isSelected) {
       setBackground(getImage(activeTile));
@@ -51,29 +154,31 @@ export const TileEmpty = ({ col, row, size, activeTile }: any) => {
       setBackground(null);
     }
   }, [isSelected, orientation, activeTile]);
-
   useEffect(() => {
     if (isSelected) {
       setRotation(calculateRotation(orientation));
     }
   }, [isSelected, orientation]);
-
   useEffect(() => {
     document.body.style.cursor = hovered ? "pointer" : "auto";
   }, [hovered]);
-
   useEffect(() => {
     if (!isHovered) {
       handlePointerLeave();
     }
   }, [isHovered]);
 
+  // useEffect(() => {
+  //   if (isSelected && activeTile) {
+  //     setValid(isValid || false);
+  //   }
+  // }, [isSelected, isValid]);
+
   const handleMeshClick = () => {
     setSelectedTile({ col, row });
     setX(col);
     setY(row);
   };
-
   const handlePointerEnter = () => {
     setHovered(true);
     setHoveredTile({ col, row });
@@ -84,7 +189,6 @@ export const TileEmpty = ({ col, row, size, activeTile }: any) => {
       setBackground(null);
     }
   };
-
   const handlePointerLeave = () => {
     setHovered(false);
     if (isSelected && activeTile) {
@@ -94,7 +198,6 @@ export const TileEmpty = ({ col, row, size, activeTile }: any) => {
       setBackground(null);
     }
   };
-
   const position = useMemo(() => {
     const position = getSquarePosition({
       row: row - offset + other_offset,
@@ -103,7 +206,6 @@ export const TileEmpty = ({ col, row, size, activeTile }: any) => {
     });
     return position;
   }, []);
-
   return (
     <>
       {texture && (
@@ -116,13 +218,15 @@ export const TileEmpty = ({ col, row, size, activeTile }: any) => {
           geometry={squareGeometry}
         >
           <meshStandardMaterial
-            emissive={"#FFFFFF"}
-            emissiveIntensity={0.1}
+            // emissive={isValid ? "green" : "red"}
+            emissive={"white"}
+            emissiveIntensity={0.3}
             map={texture}
           />
         </mesh>
       )}
       {!texture && (
+        // @ts-ignore
         <mesh
           onPointerEnter={handlePointerEnter}
           onPointerLeave={handlePointerLeave}
