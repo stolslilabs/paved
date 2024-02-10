55c090720f65159b4bf4b2dfe9e8fa98254a398f
bal7hazar <bal7hazar@proton.me>
2024-02-10 10:17:47 +0100

    💄 Code style

--- react-app/src/ui/containers/Hand.tsx
+++ react-app/src/ui/containers/Hand.tsx
@@ -11,7 +11,7 @@ export const Hand = () => {
   return (
     <div className="absolute right-6 bottom-6 z-30 w-64 h-64">
       <div className="flex top-3 right-0 absolute justify-between space-x-1">
-      <Button variant={"default"} size={"icon"}>
+        <Button variant={"default"} size={"icon"}>
           <FontAwesomeIcon icon={faXmark} />
         </Button>
         <Confirm />
