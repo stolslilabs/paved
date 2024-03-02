68350321cc96fc1c6718aabb01fca110f9000265
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-03-02 22:34:20 +1100

    add music, fix camera

--- react-app/src/ui/containers/LeftHeader.tsx
+++ react-app/src/ui/containers/LeftHeader.tsx
@@ -1,8 +1,11 @@
+import { MusicPlayer } from "../components/MusicPlayer";
 import { Scoreboard } from "../components/Scoreboard";
 
 export const LeftHeader = () => {
   return (
-    <div className="z-20 flex justify-between items-center absolute top-0 left-0 uppercase px-4 text-black">
+    <div className="z-20 flex justify-between items-center absolute top-0 left-0 uppercase px-4 text-black flex-col p-4">
+      <MusicPlayer />
+
       <Scoreboard />
     </div>
   );
