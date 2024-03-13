0c233825fbc6fb6a8487e2e15cd17cba6a653429
bal7hazar <bal7hazar@proton.me>
2024-03-13 13:27:28 +0100

    📝 Update doc config

--- book/vocs.config.ts
+++ book/vocs.config.ts
@@ -127,7 +127,7 @@ export default defineConfig({
       "https://github.com/stolslilabs/paved",
     text: "Edit on GitHub",
   },
-  ogImageUrl: 'https://paved-doc.vercel.app/%logo',
+  ogImageUrl: 'https://paved-doc.vercel.app/api/og?logo=%logo&title=%title&description=%description',
   description: "Paved in order!",
   sponsors: [
     {
