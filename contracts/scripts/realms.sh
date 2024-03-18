191953e31eb10c71ea5a636c45122ae64ddf4a1f
bal7hazar <bal7hazar@proton.me>
2024-03-18 18:35:24 +0100

    🚀 Deploy on Realms

---
+++ contracts/scripts/realms.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -euo pipefail
+pushd $(dirname "$0")/..
+
+export STARKNET_RPC_URL="https://api.cartridge.gg/x/realms/katana";
+
+export DOJO_WORLD_ADDRESS=$(cat ./target/realms/manifest.json | jq -r '.world.address')
+
+export HOST_ADDRESS=$(cat ./target/realms/manifest.json | jq -r '.contracts[] | select(.name == "paved::systems::host::host" ).address')
+export MANAGE_ADDRESS=$(cat ./target/realms/manifest.json | jq -r '.contracts[] | select(.name == "paved::systems::manage::manage" ).address')
+export PLAY_ADDRESS=$(cat ./target/realms/manifest.json | jq -r '.contracts[] | select(.name == "paved::systems::play::play" ).address')
+
+echo "---------------------------------------------------------------------------"
+echo world : $DOJO_WORLD_ADDRESS 
+echo " "
+echo host : $HOST_ADDRESS
+echo manage : $MANAGE_ADDRESS
+echo play : $PLAY_ADDRESS
+echo "---------------------------------------------------------------------------"
+
+# enable system -> component authorizations
+MODELS=("Game" "Player" "PlayerName" "Builder" "BuilderPosition" "Team" "Tile" "TilePosition" "Character" "CharacterPosition")
+ACTIONS=($HOST_ADDRESS $MANAGE_ADDRESS $PLAY_ADDRESS)
+
+command="sozo auth grant writer "
+for model in "${MODELS[@]}"; do
+    for action in "${ACTIONS[@]}"; do
+        command+="$model,$action "
+    done
+done
+eval "$command"
+
+echo "Default authorizations have been successfully set."
