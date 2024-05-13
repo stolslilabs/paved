e0c8491b6cd0b41340eea2d052f9d973d04987ad
bal7hazar <bal7hazar@proton.me>
2024-05-13 09:43:29 +0200

    🔧 Swtich from burner to wallet

--- app/src/dojo/useDojo.tsx
+++ app/src/dojo/useDojo.tsx
@@ -1,13 +1,15 @@
 import { useContext } from "react";
 import { DojoContext } from "./context";
+import { useAccount } from "@starknet-react/core";
 
 export const useDojo = () => {
+  const { account } = useAccount();
   const context = useContext(DojoContext);
   if (!context)
     throw new Error("The `useDojo` hook must be used within a `DojoProvider`");
 
   return {
     setup: context,
-    account: context.account,
+    account: { account }, //context.account,
   };
 };
