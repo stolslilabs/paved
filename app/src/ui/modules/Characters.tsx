a434ab801c95f0a51504fa64bbdfae64605e9974
redrocket94 <oliver.duedam@gmail.com>
2024-06-28 03:08:02 +0100

    chore: Update Characters component with responsive layout adjustments for different screen sizes

--- app/src/ui/modules/Characters.tsx
+++ app/src/ui/modules/Characters.tsx
@@ -25,9 +25,9 @@ export const Characters = () => {
 
   return (
     <footer className="z-20 flex flex-col-reverse items-start absolute bottom-2 left-2 pointer-events-none
-                   sm:flex-row sm:justify-between sm:items-center sm:bottom-4 sm:left-1/2 sm:-translate-x-1/2">
+                   md:flex-row md:justify-between md:items-center md:bottom-4 md:left-1/2 md:-translate-x-1/2">
       <div className="flex flex-col-reverse items-start gap-4
-                  sm:flex-row sm:flex-wrap sm:justify-center sm:items-center sm:grow sm:gap-4 lg:gap-8">
+                  md:flex-row md:flex-wrap md:justify-center md:items-center md:grow md:gap-4 lg:gap-8">
         {characters.map(({ status }, index) => (
           <Character key={index} index={index} enable={status} />
         ))}
