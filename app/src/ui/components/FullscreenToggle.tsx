40aad79bc4a59529c8a73f10aadfe2eb3f83faa4
redrocket94 <oliver.duedam@gmail.com>
2024-07-09 02:31:49 +0100

    fix: fix FullscreenToggle visibility

--- app/src/ui/components/FullscreenToggle.tsx
+++ app/src/ui/components/FullscreenToggle.tsx
@@ -6,29 +6,17 @@ export const FullscreenToggle = () => {
     const [isFullscreen, setIsFullscreen] = useState(false);
 
     useEffect(() => {
-        const handleFullscreenChange = () => {
-            setIsFullscreen(document.fullscreenElement !== null);
-        };
-
+        const handleFullscreenChange = () => setIsFullscreen(document.fullscreenElement !== null);
         document.addEventListener('fullscreenchange', handleFullscreenChange);
 
-        return () => {
-            document.removeEventListener('fullscreenchange', handleFullscreenChange);
-        };
+        return () => document.removeEventListener('fullscreenchange', handleFullscreenChange);
     }, []);
-    const handleToggle = () => {
-        if (!document.fullscreenElement) {
-            document.documentElement.requestFullscreen();
-        } else {
-            if (document.exitFullscreen) {
-                document.exitFullscreen();
-            }
-        }
-    }
+
+    const handleToggle = () => !document.fullscreenElement ? document.documentElement.requestFullscreen() : document?.exitFullscreen()
 
     const FullscreenStateIcon = isFullscreen ? ShrinkIcon : ExpandIcon
 
-    return (
+    return document.fullscreenEnabled && (
         <Button
             variant="default"
             className="w-full"
