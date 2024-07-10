07a4a5c84112df255fb7dbb0ba99c1034717bc45
redrocket94 <oliver.duedam@gmail.com>
2024-07-10 05:25:38 +0100

    refactor: left-align characters on all mobile devices

--- app/src/ui/modules/Characters.tsx
+++ app/src/ui/modules/Characters.tsx
@@ -4,6 +4,7 @@ import { useMemo } from "react";
 import { useQueryParams } from "../../hooks/useQueryParams";
 import { useDojo } from "@/dojo/useDojo";
 import { useBuilder } from "@/hooks/useBuilder";
+import { isMobile } from "react-device-detect";
 
 export const Characters = () => {
   const { gameId } = useQueryParams();
@@ -25,12 +26,12 @@ export const Characters = () => {
 
   return (
     <footer
-      className="z-20 flex flex-col-reverse items-start absolute bottom-2 left-2 pointer-events-none
-                   md:flex-row md:justify-between md:items-center md:bottom-4 md:left-1/2 md:-translate-x-1/2"
+      className={`z-20 flex flex-col-reverse items-start absolute bottom-2 left-2 pointer-events-none
+                   ${!isMobile && "md:flex-row md:justify-between md:items-center md:bottom-4 md:left-1/2 md:-translate-x-1/2"}`}
     >
       <div
-        className="flex flex-col-reverse items-start gap-4
-                  md:flex-row md:flex-wrap md:justify-center md:items-center md:grow md:gap-4 lg:gap-8"
+        className={`flex flex-col-reverse items-start gap-4
+                  ${!isMobile && "md:flex-row md:flex-wrap md:justify-center md:items-center md:grow md:gap-4 lg:gap-8"}`}
       >
         {characters.map(({ status }, index) => (
           <Character key={index} index={index} enable={status} />
