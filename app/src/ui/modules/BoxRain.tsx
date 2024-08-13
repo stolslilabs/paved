bb8c62c51b9e544c198828a750023e26357d4d78
redrocket94 <oliver.duedam@gmail.com>
2024-08-13 15:03:43 +0100

    refactor: removed redundant styling from boxrain

--- app/src/ui/modules/BoxRain.tsx
+++ app/src/ui/modules/BoxRain.tsx
@@ -70,7 +70,7 @@ const BoxRainScene = () => {
   );
 
   return (
-    <div className="absolute inset-0 overflow-hidden pointer-events-none">
+    <div className="absolute inset-0 pointer-events-none">
       <Canvas style={{ width: '100%', height: '100%' }}>
         <ambientLight />
         <directionalLight
