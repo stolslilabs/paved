0d32abbd0bc933ed595279d8022e368d8d702ca7
redrocket94 <oliver.duedam@gmail.com>
2024-08-18 17:47:59 +0100

    feat: update useTutorial hook to save/load localStorage by game id

--- app/src/hooks/useTutorial.tsx
+++ app/src/hooks/useTutorial.tsx
@@ -1,8 +1,13 @@
+import { useQueryParams } from "./useQueryParams";
+
+// TODO: Clear on controller version bump 
 export const useTutorial = () => {
+    const { gameId } = useQueryParams();
+
     const firstStep = 1
     const maxSteps = 5
 
-    const getCurrentStep = () => localStorage.getItem('tutorialStep');
+    const getCurrentStep = () => localStorage.getItem(`tutorial-${gameId}`);
 
     return {
         getCurrentStep
