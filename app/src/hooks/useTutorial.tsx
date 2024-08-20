88a6933c9233e5cbc2a389100fd5bdc54600d645
redrocket94 <oliver.duedam@gmail.com>
2024-08-20 17:52:52 +0100

    feat: added useTutorial hook basics

--- app/src/hooks/useTutorial.tsx
+++ app/src/hooks/useTutorial.tsx
@@ -1,15 +1,60 @@
+import { ModeType } from "@/dojo/game/types/mode";
+import { useLobby } from "./useLobby";
 import { useQueryParams } from "./useQueryParams";
+import { useEffect, useState } from "react";
+
+
+const markedTiles = [
+    { x: -3, y: 0 },
+    { x: -3, y: -3 },
+    { x: -3, y: -6 },
+    { x: 0, y: -3 },
+    { x: 0, y: -6 },
+    { x: 3, y: -6 },
+    { x: 3, y: -3 },
+    { x: 3, y: 0 },
+]
+
+const firstStep = 0
+const maxSteps = 9
 
 // TODO: Clear on controller version bump 
 export const useTutorial = () => {
     const { gameId } = useQueryParams();
+    const { gameMode } = useLobby();
 
-    const firstStep = 1
-    const maxSteps = 5
+    const [step, setStep] = useState(firstStep)
 
-    const getCurrentStep = () => localStorage.getItem(`tutorial-${gameId}`);
+    useEffect(() => {
+        const handleTutorialAdvance = (e: CustomEvent<number>) => {
+            setStep(e.detail)
+        }
+
+        window.addEventListener('tutorial-advance', handleTutorialAdvance as EventListener)
+
+        return () => {
+            window.removeEventListener('tutorial-advance', handleTutorialAdvance as EventListener)
+        }
+    }, [])
+
+
+    const getCurrentStep = () => localStorage.getItem(`tutorial-${gameId}`) ?? '0';
+
+    const currentStepTile = () => markedTiles[parseInt(getCurrentStep())]
+
+    const advanceStep = () => {
+        if (gameMode.value !== ModeType.Tutorial) return
+
+        localStorage.setItem(`tutorial-${gameId}`, step.toString())
+
+        dispatchEvent(new CustomEvent('tutorial-advance', { detail: step + 1 }));
+    }
 
     return {
-        getCurrentStep
+        getCurrentStep,
+        currentStepTile,
+        advanceStep,
+        firstStep,
+        maxSteps
     }
 }
