5a935f301edc961917ba584bf5df2fe362fc69ff
redrocket94 <oliver.duedam@gmail.com>
2024-08-23 05:52:14 +0100

    feat: attach TutorialDialog text to current step

--- app/src/ui/components/dom/TutorialDialog.tsx
+++ app/src/ui/components/dom/TutorialDialog.tsx
@@ -1,16 +1,13 @@
 import { useTutorial } from "@/hooks/useTutorial"
 
 export const TutorialDialog = () => {
-    const { step } = useTutorial()
-    return (
+    const { currentTutorialStage } = useTutorial()
+
+    return currentTutorialStage().textboxText && (
         <div id="tutorial-dialog" className="h-full w-full self-end bg-gray-300 rounded p-2 z-40 pointer-events-auto">
             {/* Add your tutorial content here */}
-            <h4 className="font-bold text-sm m-0 mb-1">Tutorial</h4>
-            <p className="text-xs tracking-tighter">
-                FADED TILE AND HIGHLIGHTED AREAS FOR TUTORIAL
-                PURPOSES ONLY. PLEASE FOLLOW INSTRUCTIONS TO MOVE TO
-                THE NEXT STEPS.
-            </p>
+            <h4 className="font-bold text-sm m-0 mb-1">Tips</h4>
+            <p className="text-xs tracking-tighter">{currentTutorialStage().textboxText}</p>
         </div>
     )
 }
