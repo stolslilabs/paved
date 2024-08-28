382683d14dc92eaca907862634dbe9c02afaed51
redrocket94 <oliver.duedam@gmail.com>
2024-08-28 09:02:39 +0100

    feat: added partial tutorial stage progress feature

--- app/src/hooks/useTutorial.tsx
+++ app/src/hooks/useTutorial.tsx
@@ -1,15 +1,32 @@
 import { useQueryParams } from "./useQueryParams";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useGame } from "./useGame";
 import { TUTORIAL_STAGES } from "@/dojo/game/types/tutorial-stage";
+import { useGameStore } from "@/store";
+
+const coordOffset = 2147483647
+const sizeOffset = 3
 
 export const useTutorial = () => {
+
     const { gameId } = useQueryParams();
     const { game } = useGame({ gameId });
 
+    const { x, y } = useGameStore();
+
     const step = useMemo(() => (game?.built ?? 0) + (game?.discarded ?? 0), [game?.built, game?.discarded])
 
+    const [actionStep, setActionStep] = useState(0)
+
     const currentTutorialStage = useMemo(() => TUTORIAL_STAGES[step], [step])
+
+    useEffect(() => {
+        const normalizedX = (x - coordOffset) * sizeOffset
+        const normalizedY = (y - coordOffset) * sizeOffset
+        console.log(normalizedX === currentTutorialStage.markedTile.x, normalizedY === currentTutorialStage.markedTile.y)
+
+    }, [currentTutorialStage.markedTile.x, currentTutorialStage.markedTile.y, x, y])
+
 
     return {
         currentTutorialStage,
