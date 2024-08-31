2f7f1c1ee7f89bc8c1f1b3d7630937592bbfc81d
redrocket94 <oliver.duedam@gmail.com>
2024-08-31 05:56:54 +0100

    fix: load tutorial dialog only on tutorial gamemode

--- app/src/ui/components/dom/TutorialDialog.tsx
+++ app/src/ui/components/dom/TutorialDialog.tsx
@@ -1,9 +1,15 @@
+import { ModeType } from "@/dojo/game/types/mode";
+import { useGame } from "@/hooks/useGame";
+import { useQueryParams } from "@/hooks/useQueryParams";
 import { useTutorial } from "@/hooks/useTutorial"
 
 export const TutorialDialog = () => {
+    const { gameId } = useQueryParams();
+    const { game } = useGame({ gameId });
+
     const { currentTutorialStage } = useTutorial()
 
-    return currentTutorialStage?.textboxText && (
+    return currentTutorialStage?.textboxText && game?.mode.value === ModeType.Tutorial && (
         <div id="tutorial-dialog" className="h-full w-full self-end bg-gray-300 rounded p-2 z-40 pointer-events-auto">
             {/* Add your tutorial content here */}
             <h4 className="font-bold text-sm m-0 mb-1">Tips</h4>
