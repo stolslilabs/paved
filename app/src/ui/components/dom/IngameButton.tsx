92bc685b2177b0357f1b253dd0171b3ab3968bf4
redrocket94 <oliver.duedam@gmail.com>
2024-09-07 16:10:00 +0200

    fix: tooltip visibility state fix, index text removal

--- app/src/ui/components/dom/IngameButton.tsx
+++ app/src/ui/components/dom/IngameButton.tsx
@@ -11,25 +11,25 @@ type IngameButtonProps = ButtonProps & React.RefAttributes<HTMLButtonElement> & 
     icon: string
     name?: string
     side?: "left" | "right" | "top" | "bottom"
+    tutorialCondition?: boolean
     children?: ReactElement
 }
 
 export const IngameButton = React.forwardRef<HTMLButtonElement, IngameButtonProps>(
-    ({ icon, name, side = "right", children, className, ...props }, ref): JSX.Element => {
+    ({ icon, name, side = "right", tutorialCondition = false, children, className, ...props }, ref): JSX.Element => {
         const { gameId } = useQueryParams();
         const { game } = useGame({ gameId });
         const { currentTutorialStage } = useTutorial();
 
         const interactionText = currentTutorialStage?.interactionText?.get(props.id ?? "")
         const tutorialOpen = props.id && game?.mode.value === ModeType.Tutorial && interactionText
-        const interactionIndex = tutorialOpen ? Array.from(currentTutorialStage.interactionText?.keys()).indexOf(props.id ?? "") + 1 : -1;
 
         const [ephemeralOpen, setEphemeralOpen] = useState(!!tutorialOpen)
 
         const content = children ?? (
             <Button
                 ref={ref}
-                className={cn(`px-2 aspect-square size-10 xl:size-16 p-2 ${tutorialOpen ? "border-solid" : "border-none"} border-blue-500 border-2 bg-[#D2E2F1] bg-opacity-80 rounded-md pointer-events-auto flex items-center justify-center`, className)}
+                className={cn(`px-2 aspect-square size-10 xl:size-16 p-2 ${tutorialOpen && tutorialCondition ? "border-solid" : "border-none"} border-blue-500 border-2 bg-[#D2E2F1] bg-opacity-80 rounded-md pointer-events-auto flex items-center justify-center`, className)}
                 {...props}
             >
                 <img src={icon} className="w-full h-full object-contain" />
@@ -38,12 +38,20 @@ export const IngameButton = React.forwardRef<HTMLButtonElement, IngameButtonProp
 
         return name ? (
             <TooltipProvider>
-                <Tooltip open={ephemeralOpen || !!tutorialOpen} onOpenChange={setEphemeralOpen}>
+                <Tooltip
+                    open={ephemeralOpen || (!!tutorialOpen && tutorialCondition)}
+                    onOpenChange={(open) => {
+                        if (!tutorialOpen && !tutorialCondition) {
+                            console.log(open)
+                            setEphemeralOpen(open);
+                        }
+                    }}
+                >
                     <TooltipTrigger asChild>
                         {content}
                     </TooltipTrigger>
                     <TooltipContent className="bg-transparent" side={side}>
-                        <p>{tutorialOpen ? `${interactionIndex}.${interactionText ?? ""}` : name}</p>
+                        <p>{tutorialOpen && tutorialCondition ? `${interactionText ?? ""}` : name}</p>
                     </TooltipContent>
                 </Tooltip>
             </TooltipProvider>
