f1667931d168d8b5337d155d36d9af29dcd92564
redrocket94 <oliver.duedam@gmail.com>
2024-09-18 19:23:10 +0100

    refactor: cleaned up useTutorial

--- app/src/hooks/useTutorial.tsx
+++ app/src/hooks/useTutorial.tsx
@@ -1,34 +1,20 @@
 import { useQueryParams } from "./useQueryParams";
-import { useEffect, useMemo, useState } from "react";
+import { useMemo } from "react";
 import { useGame } from "./useGame";
 import { TUTORIAL_STAGES } from "@/dojo/game/types/tutorial-stage";
-import { useGameStore } from "@/store";
-
-const coordOffset = 2147483647
-const sizeOffset = 3
 
 export const useTutorial = () => {
-
     const { gameId } = useQueryParams();
     const { game } = useGame({ gameId });
 
-    const { x, y } = useGameStore();
-
     const step = useMemo(() => (game?.built ?? 0) + (game?.discarded ?? 0), [game?.built, game?.discarded])
-
-    const [actionStep, setActionStep] = useState(0)
+    const maxSteps = 8
 
     const currentTutorialStage = useMemo(() => TUTORIAL_STAGES[step], [step])
-
-    useEffect(() => {
-        const normalizedX = (x - coordOffset) * sizeOffset
-        const normalizedY = (y - coordOffset) * sizeOffset
-
-    }, [currentTutorialStage?.markedTile?.x, currentTutorialStage?.markedTile?.y, x, y])
-
 
     return {
         currentTutorialStage,
         step,
+        maxSteps
     }
 }
