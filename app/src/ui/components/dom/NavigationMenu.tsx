d0463a528ce58b18321127dd13b338c792e7c292
redrocket94 <oliver.duedam@gmail.com>
2024-10-03 16:24:27 +0200

    feat: added fullscreen button to burger menu

--- app/src/ui/components/dom/NavigationMenu.tsx
+++ app/src/ui/components/dom/NavigationMenu.tsx
@@ -28,6 +28,9 @@ import {
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/ui/elements/collapsible";
+import fullscreenOn from "/assets/icons/fullscreen-on.svg";
+import fullscreenOff from "/assets/icons/fullscreen-off.svg";
+import { useFullscreen } from "@/hooks/useFullscreen";
 
 type MenuItem = {
   name: string;
@@ -55,6 +58,8 @@ export const NavigationMenu = ({
 
   const [compositionOpen, setCompositionOpen] = useState<boolean>(false);
   const toggleMusic = () => setMuted(!muted);
+
+  const fullscreen = useFullscreen()
 
   const NavigationMenuItems: Array<MenuItem> = [
     {
@@ -95,6 +100,11 @@ export const NavigationMenu = ({
       icon: burnIcon,
       children: <Discard />,
     },
+    {
+      name: fullscreen ? "Exit Fullscreen" : "Enter Fullscreen",
+      icon: fullscreen ? fullscreenOff : fullscreenOn,
+      onClick: () => fullscreen ? document.exitFullscreen() : document.body.requestFullscreen()
+    }
   ];
 
   return !compositionOpen ? (
