dda2880e04cb499ffe13becb09fc0a9260d08e56
redrocket94 <oliver.duedam@gmail.com>
2024-10-03 14:05:25 +0200

    refactor: remove redundant components

--- app/src/ui/components/dom/TutorialCompleteDialog.tsx
+++ app/src/ui/components/dom/TutorialCompleteDialog.tsx
@@ -1,9 +1,7 @@
-import { getColor } from "@/dojo/game";
 import { ModeType } from "@/dojo/game/types/mode";
 import { useDojo } from "@/dojo/useDojo";
 import { useBuilders } from "@/hooks/useBuilders";
 import { useGame } from "@/hooks/useGame";
-import { usePlayer } from "@/hooks/usePlayer";
 import { useQueryParams } from "@/hooks/useQueryParams";
 import { useTutorial } from "@/hooks/useTutorial";
 import { useUIStore } from "@/store";
@@ -17,14 +15,14 @@ import {
   AlertDialogTitle,
 } from "@/ui/elements/alert-dialog";
 import { Button } from "@/ui/elements/button";
-import { TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/ui/elements/table";
 import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Table } from "lucide-react";
 import { useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Game as GameClass } from "@/dojo/game/models/game";
 import { TwitterShareButton } from "react-share";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/ui/elements/dialog";
+import { Leaderboard } from "../Leaderboard";
 
 export const GameCompletedDialog = () => {
   const { gameId } = useQueryParams();
@@ -76,20 +74,20 @@ const RegularDialog = () => {
   const { builders } = useBuilders({ gameId });
   const [open, setOpen] = useState(true);
 
-  const isSelf = useMemo(() => account?.address === (builders.length > 0 ? builders[0].player_id : "0x0"), [account, builders, game]);
+  const isSelf = useMemo(() => account?.address === (builders.length > 0 ? builders[0].player_id : "0x0"), [account, builders]);
 
   if (!game || !builders || !builders.length) return null;
 
   return (
-    <AlertDialog open={open && game.isOver() && isSelf} onOpenChange={setOpen}>
-      <AlertDialogContent className="bg-primary">
-        <AlertDialogHeader>
-          <AlertDialogTitle className="text-center text-xl">Leaderboard</AlertDialogTitle>
-        </AlertDialogHeader>
+    <Dialog open={open && game.isOver() && isSelf} onOpenChange={setOpen}>
+      <DialogContent className="bg-primary">
+        <DialogHeader>
+          <DialogTitle className="text-center text-xl">Leaderboard</DialogTitle>
+        </DialogHeader>
         <Description game={game} />
         <Leaderboard game={game} builders={builders} />
-      </AlertDialogContent>
-    </AlertDialog>
+      </DialogContent>
+    </Dialog>
   );
 };
 
@@ -104,7 +102,7 @@ export const Description = ({ game }: { game: GameClass }) => {
   };
 
   return (
-    <AlertDialogDescription className="flex justify-center items-center gap-3 text-xs flex-wrap">
+    <DialogDescription className="flex justify-center items-center gap-3 text-xs flex-wrap">
       <div className="w-full text-center text-3xl">PUZZLE COMPLETE!</div>
       <div className="w-full text-center">
         Well paved, adventurer ⚒️ <br />
@@ -122,7 +120,7 @@ export const Description = ({ game }: { game: GameClass }) => {
       </Button>
 
       <Share score={game.score} />
-    </AlertDialogDescription>
+    </DialogDescription>
   );
 };
 
@@ -148,56 +146,5 @@ Play now 👇
         <p>Share</p>
       </Button>
     </TwitterShareButton>
-  );
-};
-
-export const Leaderboard = ({
-  game,
-  builders,
-}: {
-  game: any;
-  builders: any[];
-}) => {
-  return (
-    <Table className="text-xs">
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Rank</TableHead>
-          <TableHead className="w-[100px]">Score</TableHead>
-          <TableHead>Name</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <PlayerRow rank={1} score={game?.score || 0} builder={builders[0]} />
-      </TableBody>
-    </Table>
-  );
-};
-
-export const PlayerRow = ({
-  rank,
-  score,
-  builder,
-}: {
-  rank: number;
-  score: number;
-  builder: any;
-}) => {
-  const { player } = usePlayer({ playerId: builder.player_id });
-  const name = player?.name || "";
-  const backgroundColor = useMemo(
-    () => getColor(`0x${builder.player_id.toString(16)}`),
-    [builder],
-  );
-
-  return (
-    <TableRow>
-      <TableCell className="">{rank}</TableCell>
-      <TableCell>{score}</TableCell>
-      <TableCell className="flex gap-2 text-ellipsis">
-        <div className="rounded-full w-4 h-4" style={{ backgroundColor }} />
-        {name}
-      </TableCell>
-    </TableRow>
   );
 };
