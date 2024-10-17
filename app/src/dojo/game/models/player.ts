0b44ffe99686141a7ebfd53d3ebc3b330b90b124
redrocket94 <oliver.duedam@gmail.com>
2024-10-17 02:05:56 +0100

    feat: updated player properties

--- app/src/dojo/game/models/player.ts
+++ app/src/dojo/game/models/player.ts
@@ -4,14 +4,10 @@ import { shortString } from "starknet";
 export class Player {
   public id: string;
   public name: string;
-  public score: number;
-  public paved: number;
 
   constructor(player: ComponentValue) {
     this.id = `0x${player.id.toString(16).replace("0x", "")}`;
     this.name = shortString.decodeShortString(player.name);
-    this.score = player.score;
-    this.paved = player.paved;
   }
 
   public getShortName(): string {
