fb2e818cf7088cdc4a8bb41c42ad1c81b8375d17
redrocket94 <oliver.duedam@gmail.com>
2024-10-23 10:04:57 +0100

    fix: centering on CreateGame Tutorial version

--- app/src/ui/components/CreateGame.tsx
+++ app/src/ui/components/CreateGame.tsx
@@ -5,13 +5,12 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/ui/elements/tooltip";
-import { Account } from "starknet";
 import { usePlayer } from "@/hooks/usePlayer";
 import { useState } from "react";
 import { Mode, ModeType } from "@/dojo/game/types/mode";
 import { useLobby } from "@/hooks/useLobby";
 import { Button } from "../elements/button";
-import { DuelDialog } from "./dom/DuelDialog";
+import { DuelDialog } from "./dom/dialogs/duels/DuelDialog";
 
 export const CreateGame = ({ mode }: { mode: Mode }) => {
   const {
@@ -30,7 +29,7 @@ export const CreateGame = ({ mode }: { mode: Mode }) => {
     if (!player) return;
     setLoading(true);
     await create_game({
-      account: account as Account,
+      account: account,
       mode: mode,
     });
     if (gameMode.value === ModeType.Tutorial) return;
@@ -45,7 +44,7 @@ export const CreateGame = ({ mode }: { mode: Mode }) => {
             <DuelDialog playerName={player?.name} />
           ) : (
             <Button
-              className="tracking-[0.25rem] shadow-lg hover:bg-secondary text-xs lg:text-sm px-4 py-4 self-end lg:p-6"
+              className="tracking-[0.25rem] self-center shadow-lg hover:bg-secondary text-xs lg:text-sm px-4 py-4 lg:p-6"
               loading={loading}
               disabled={!player || loading}
               onClick={handleClick}
